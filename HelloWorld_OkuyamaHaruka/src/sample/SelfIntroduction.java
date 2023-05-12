454c23602bef556333ac272ff73eed50f6f49cd7
ohhar <ohhar@okko>
2023-05-12 10:27:41 +0900

    1.3 新規作成

---
+++ HelloWorld_OkuyamaHaruka/src/sample/SelfIntroduction.java
@@ -0,0 +1,22 @@
+package sample;
+
+public class SelfIntroduction {
+
+	public static void main(String[] args) {
+		//氏名
+		String name = "奥山 陽加";
+		//年齢
+		int age = 23;
+		//趣味
+		String hobby = "読書";
+
+		/*
+		 * 自己紹介を出力する処理
+		 */
+		
+		System.out.println("私の名前は" + name + "です。");
+		System.out.println("年齢は" + age + "歳です。");
+		System.out.println("趣味は" + hobby + "です。");
+	}
+
+}
